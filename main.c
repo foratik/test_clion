5f46abd701a469f3465630d396e43b6fa5c42d0c
Saeed.F <saeed.forati.k@gmail.com>
2022-12-02 21:55:53 +0330

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int n,a[];
+    scanf("%d\n",&n);
+    for (int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for (int k=1;k<=n;k++){
+        for(int)
+    }
+
+
+    return 0;
+}
